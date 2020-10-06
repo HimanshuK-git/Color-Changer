f158b095480f97f90be4ababed44af8d12f3a62f
HimanshuK-git <66906499+HimanshuK-git@users.noreply.github.com>
2020-10-06 19:25:18 +0530

    Color changer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChangerComponent } from './changer/changer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangerComponent
   ],
   imports: [
     BrowserModule,
